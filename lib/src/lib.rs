7ae770aae7783d6f068ab92472bc22d2009def3f
Ansh1902396 <rudransh_shinghal@yahoo.co.in>
2025-06-17 17:31:36 +0800

    chore: fix cargo file

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,4 +1,5 @@
 use alloy_sol_types::sol;
+use serde::{Deserialize, Serialize};
 
 sol! {
     /// The public values encoded as a struct that can be easily deserialized inside Solidity.
@@ -8,6 +9,19 @@ sol! {
         uint32 b;
     }
 }
+
+#[derive(Serialize, Deserialize)]
+pub struct Order {
+    pub side:      u8,        // 0 = buy , 1 = sell
+    pub token_in:  [u8; 20],
+    pub token_out: [u8; 20],
+    pub qty:       u128,      // amount of token_in
+    pub limit_px:  u128,      // price * 1e18
+    pub deadline:  u64,
+    pub nonce:     u32,
+}
+
+
 
 /// Compute the n'th fibonacci number (wrapping around on overflows), using normal Rust code.
 pub fn fibonacci(n: u32) -> (u32, u32) {
